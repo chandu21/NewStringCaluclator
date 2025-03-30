0665a2ec7d471243b3bff04068b33b73ade06287
chandu21 <chandu.basina23@gmail.com>
2025-03-31 00:03:16 +0530

    positive testcase for valid input

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe StringCalculator, type: :model do
   it 'returns 0 for an empty string' do
     expect(StringCalculator.add("")).to eq(0)
   end
+
+  it 'returns sum of numbers separated by commas' do
+    expect(StringCalculator.add("1,2")).to eq(3)
+  end
 end
