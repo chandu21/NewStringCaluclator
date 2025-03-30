d17a2f8766c957abfe772c4f12ae3edc890965fb
chandu21 <chandu.basina23@gmail.com>
2025-03-30 22:18:34 +0530

    Added exception for negetive values

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -3,6 +3,8 @@ class StringCalculator < ApplicationRecord
   	 return 0 if num.blank?
      delimiter = ",|\n"
      num_list = num.split(Regexp.union(delimiter))
+     negetive_values = num_list.select { |num| num < 0 }
+     raise "Negatives not allowed" if negatives.present?
      num_list.sum
   end
 end
