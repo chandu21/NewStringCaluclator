2b330d2e9748bce60ce6c4a2a004a7c4d2c70fc8
chandu21 <chandu.basina23@gmail.com>
2025-03-30 22:12:42 +0530

    regular expression splitting based on delimeter

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,5 +1,8 @@
 class StringCalculator < ApplicationRecord
-  def self.add(numbers)
-  	return 1
+  def self.add(num)
+  	 return 0 if num.blank?
+     delimiter = ",|\n"
+     num_list = num.split(Regexp.union(delimiter))
+     num_list.sum
   end
 end
